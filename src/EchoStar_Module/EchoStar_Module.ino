9f347b71f11986bbd0c61d9facd3ab60cf4bfe9d
BASSO-Jostin <bassojostin@gmail.com>
2025-01-29 23:59:55 +0100

    Adding source codes

---
+++ src/EchoStar_Module/EchoStar_Module.ino
@@ -0,0 +1,187 @@
+#include "STM32LowPower.h"
+#include <STM32RTC.h>
+#include "es_delay.h"
+#include <SoftwareSerial.h>
+
+
+#define EchoStar_Activation PB5  //Wake_up pin activation
+
+int acknowledgment = 0;
+
+#define SWITCH_REVERSING_CONTROL_DEFAULT_VALUE true
+volatile int switch_reversing_control = SWITCH_REVERSING_CONTROL_DEFAULT_VALUE;
+
+char buffer[255];
+uint8_t buffer_len = 0;
+
+
+void setup(void) {
+  // put your setup code here, to run once:
+  DELAY_MANAGER.init();
+  pinMode(LED_BUILTIN, OUTPUT);
+
+#if defined(ECHOSTAR_PWR_ENABLE_PIN)
+  pinMode(ECHOSTAR_PWR_ENABLE_PIN, OUTPUT);
+  digitalWrite(ECHOSTAR_PWR_ENABLE_PIN, HIGH);
+#endif
+
+#if defined(DPDT_PWR_ENABLE_PIN)
+  pinMode(DPDT_PWR_ENABLE_PIN, OUTPUT);
+  digitalWrite(DPDT_PWR_ENABLE_PIN, HIGH);
+#endif
+  pinMode(DPDT_CTRL_PIN, OUTPUT);
+  digitalWrite(DPDT_CTRL_PIN, HIGH);
+
+  pinMode(ECHOSTAR_SWCTRL_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(ECHOSTAR_SWCTRL_PIN), swctrl_change_isr, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(EchoStar_Activation), button_1_isr, RISING);
+
+  //USB_SERIAL.begin(115200);
+  /*while (!USB_SERIAL)
+    ;*/
+
+  //USB_SERIAL.println("Starting...");
+
+  do_switch_ctrl_update();
+
+  ECHOSTAR_SERIAL.begin(115200);
+
+  //Initialize the serial mode
+  Serial2.begin(115200);
+
+  //AT JOIN command in order to be connected to the satellite
+  ECHOSTAR_SERIAL.write("AT+JOIN\r\n");
+
+  digitalWrite(LED_BUILTIN, LOW);
+  //EM2050_soft_sleep_enable(); //go to sleep mode
+  delay(500);
+}
+
+void loop(void) {
+
+  USB_SERIAL.println("0");
+  digitalWrite(LED_BUILTIN, LOW);
+  EM2050_soft_sleep_enable();
+  DELAY_MANAGER.delay_ms(60000);
+  EM2050_soft_sleep_disable();
+  digitalWrite(LED_BUILTIN, HIGH);
+
+
+  char command_packet[150];
+  memset(command_packet, 0, 150);
+
+  //When Serial2 receives data from the relay
+  while (Serial2.available()) {
+    acknowledgment = 1;  //When data is received but the paquet is not complete
+    buffer[buffer_len++] = Serial2.read();
+    //To see what is received
+    //USB_SERIAL.println(buffer_len);
+
+    if (buffer_len == 38) {
+      acknowledgment = 2;  //the paquet is complete
+
+      char packet[50];
+      int packet_len = 0;
+
+      //To save dev_Addr in packet
+      packet[packet_len++] = buffer[8];
+      packet[packet_len++] = buffer[9];
+      packet[packet_len++] = buffer[6];
+      packet[packet_len++] = buffer[7];
+      packet[packet_len++] = buffer[4];
+      packet[packet_len++] = buffer[5];
+      packet[packet_len++] = buffer[2];
+      packet[packet_len++] = buffer[3];
+
+      //To save payload in packet
+      for (int i = 18; i < 30; i++) {
+        packet[packet_len++] = buffer[i];
+      }
+
+      // Buffer for AT command in char
+      char command[150];
+
+      sprintf(command, "AT+SEND=1,0,8,0,%s\r\n", buffer);
+      USB_SERIAL.println("To see what is in the buffer ");
+      USB_SERIAL.println(command);
+      delay(100);
+
+      sprintf(command_packet, "AT+SEND=1,0,8,0,%s\r\n", packet);
+      USB_SERIAL.println("To see what is sent ");
+      USB_SERIAL.println(command_packet);
+
+      //Sending packet with AT + SEND command to the satellite
+      ECHOSTAR_SERIAL.println(command_packet);
+      buffer_len = 0;  //Reset the counter after sending
+
+
+      delay(500);
+    }
+  }
+
+  if (acknowledgment == 1) {
+    Serial2.write(1);  //1 for NAK
+    USB_SERIAL.println("I am 1 ");
+    acknowledgment=0;
+  } 
+  else if (acknowledgment == 2) {
+    Serial2.write(2);  //2 for ACK
+    USB_SERIAL.println("I am 2 ");
+    acknowledgment=0;
+  }
+
+  delay(1000);
+}
+
+
+void swctrl_change_isr(void) {
+
+  do_switch_ctrl_update();
+}
+
+void do_switch_ctrl_update(void) {
+  if (digitalRead(ECHOSTAR_SWCTRL_PIN) == LOW) {
+
+    digitalWrite(DPDT_CTRL_PIN, switch_reversing_control ? HIGH : LOW);
+    digitalWrite(LED_BUILTIN, switch_reversing_control ? HIGH : LOW);
+  } else {
+    digitalWrite(DPDT_CTRL_PIN, switch_reversing_control ? LOW : HIGH);
+    digitalWrite(LED_BUILTIN, switch_reversing_control ? LOW : HIGH);
+  }
+}
+
+void sleepEchoStar(int sleeping_time_ms) {
+  if (sleeping_time_ms < 1000) {
+    delay(1000);
+  } else {
+    // Put the board to sleep
+    EM2050_soft_sleep_enable();
+    //mcu_sleep(sleeping_time_ms * 1000);
+    EM2050_soft_sleep_disable();
+  }
+}
+
+
+void EM2050_soft_sleep_enable(void) {
+  pinMode(ECHOSTAR_RTS_PIN, OUTPUT);
+  digitalWrite(ECHOSTAR_RTS_PIN, HIGH);
+  delay(50);
+}
+
+void EM2050_soft_sleep_disable(void) {
+  pinMode(ECHOSTAR_RTS_PIN, INPUT);
+  delay(50);
+}
+
+void Interruption_delay() {
+
+  //USB_SERIAL.println("Je suis ici");
+}
+
+/*void mcu_sleep(uint32_t sleep_duration_s) {
+  rtc.setAlarmEpoch(rtc.getEpoch() + sleep_duration_s);
+  LowPower.deepSleep();
+}*/
+
+void button_1_isr(void) {
+}
---
+++ src/Relay/Relay.ino
@@ -0,0 +1,187 @@
+/*
+ *     __          ____        _____                       __    _ __
+ *    / /   ____  / __ \____ _/ ___/____  ____ _________  / /   (_) /_
+ *   / /   / __ \/ /_/ / __ `/\__ \/ __ \/ __ `/ ___/ _ \/ /   / / __ \
+ *  / /___/ /_/ / _, _/ /_/ /___/ / /_/ / /_/ / /__/  __/ /___/ / /_/ /
+ * /_____/\____/_/ |_|\__,_//____/ .___/\__,_/\___/\___/_____/_/_.___/
+ *                              /_/
+ * Author: jostin, m1nhle, mtnguyen
+ * Lib jointy developed by UCA & RFThings
+ */
+
+#include <RFThings.h>
+#include <rfthings_sx126x.h>
+#include <RTC.h>
+#include <time.h>
+
+// Device information (For uploading relay status)
+static uint8_t nwkS_key[] = { 0x2B, 0x65, 0xA2, 0x97, 0x60, 0xB9, 0x15, 0xDE, 0x10, 0x59, 0x67, 0x75, 0x16, 0xA8, 0xE9, 0x1D };  // <-- MODIFY THIS INFORMATION ACCORDING TO YOUR USECASE
+static uint8_t appS_key[] = { 0x89, 0xCE, 0xC1, 0x77, 0x53, 0x32, 0x7E, 0x95, 0xE6, 0x63, 0x90, 0x4F, 0x7F, 0x04, 0xFB, 0x70 };  // <-- MODIFY THIS INFORMATION ACCORDING TO YOUR USECASE
+static uint8_t dev_addr[] = { 0x26, 0x0B, 0xBD, 0xFB };                                                                          // <-- MODIFY THIS INFORMATION ACCORDING TO YOUR USECASE
+
+// DKPlatium
+rfthings_sx126x sx126x(E22_NSS, E22_NRST, E22_BUSY, E22_DIO1, E22_RXEN);
+rft_status_t status;
+rft_status_t status_send_to_GW;
+
+#define EchoStarActivation 15  //PA5 Pin used to wake up EchoStar Module
+
+// Relay params (Listen to all devices meet this LoRaPHY Params, NO FILTER)
+rft_lora_params_t relay_lora_params;
+rft_lora_params_t send_toGW_lora_params;
+byte payload[255];
+uint32_t payload_len;
+
+char hexPayload[50];  // Every byte = 2 caracters hex + '\0'
+
+void setup(void) {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(50);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+
+
+  pinMode(LS_GPS_ENABLE, OUTPUT);
+  pinMode(LS_VERSION_ENABLE, OUTPUT);
+  pinMode(LS_GPS_V_BCKP, OUTPUT);
+  pinMode(SD_ON_OFF, OUTPUT);
+
+
+  // Inititialize the LoRa Module SX126x
+  status = sx126x.init(RFT_REGION_EU863_870);
+  // Serial.print("LoRa SX126x: ");
+  // Serial.println(rft_status_to_str(status));
+
+  // Set up LoRaWAN key (ABP by default)
+  sx126x.set_lorawan_activation_type(RFT_LORAWAN_ACTIVATION_TYPE_ABP);
+  sx126x.set_application_session_key(appS_key);
+  sx126x.set_network_session_key(nwkS_key);
+  sx126x.set_device_address(dev_addr);
+  sx126x.set_rx1_delay(1000);
+
+  // Setting Relay LoRa Params
+  relay_lora_params.frequency = 866600000;
+  relay_lora_params.spreading_factor = RFT_LORA_SPREADING_FACTOR_7;
+  relay_lora_params.bandwidth = RFT_LORA_BANDWIDTH_125KHZ;
+  relay_lora_params.coding_rate = RFT_LORA_CODING_RATE_4_5;
+  relay_lora_params.syncword = RFT_LORA_SYNCWORD_PUBLIC;
+  relay_lora_params.relay_sleep_interval_us = 1E6;  // 1 second
+  relay_lora_params.relay_rx_symbol = 5;
+  relay_lora_params.relay_max_rx_packet_length = 120;
+
+  //Initialize the Serial mode
+  Serial3.begin(115200);
+  Serial.begin(115200);
+
+  pinMode(EchoStarActivation, OUTPUT);
+
+  delay(2000);
+}
+
+void loop(void) {
+  //sx126x.set_frequency(866600000);
+  Serial.println("Loop");
+
+  digitalWrite(EchoStarActivation, LOW);  //Put the the module EchoStar on sleep
+
+  switch (sx126x.relay(&relay_lora_params, payload, payload_len, NULL, /*NULL*/sleepRelay)) {
+    case RFT_STATUS_OK:
+      if (payload_len > 0) {
+        //Serial.begin(115200);
+        digitalWrite(EchoStarActivation, HIGH);  //Wake up the module EchoStar
+        delay(1000);                             //Delay before forwarding
+
+        sx126x.set_tx_power(14);
+
+
+        memset(hexPayload, 0, 50);
+        for (int i = 0; i < 19; i++) {
+          sprintf(&hexPayload[2 * i], "%02X", payload[i]);  // Convert every by to hex
+        }
+
+        //The forward to the EchoStar module by using the serial mode
+        Serial3.write(hexPayload);
+
+        Serial.println("Received");
+
+        //What i am going to add
+        while (!Serial3.available()) {
+          int sortir = 1;
+          Serial.println("Waiting acknowledgment");
+
+          while (Serial3.available()) {
+            sortir = 2;
+
+
+            int lecture = Serial3.read();
+            if (lecture == 1) {  //1 for NAK
+              Serial3.write(hexPayload);
+            } else if (lecture == 2) {  //2 for ACK
+              Serial.println("Received acknowledgment");
+              digitalWrite(LED_BUILTIN, HIGH);
+              delay(125);
+              digitalWrite(LED_BUILTIN, LOW);
+              delay(50);
+              digitalWrite(LED_BUILTIN, HIGH);
+              delay(125);
+              digitalWrite(LED_BUILTIN, LOW);
+            }
+          }
+
+          if (sortir == 2) {
+            break;
+          }
+        }
+
+        /*digitalWrite(LED_BUILTIN, HIGH);
+        delay(125);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(50);
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(125);
+        digitalWrite(LED_BUILTIN, LOW);*/
+
+        delay(1000);
+      } else {
+        // Serial.println("[Relay] ERROR: Unknown ERROR!");
+      }
+      break;
+    case RFT_STATUS_RX_TIMEOUT:
+      // Serial.println("[Relay] ERROR: Preamble detected, RX timeout!");
+      break;
+    case RFT_STATUS_PREAMBLE_DETECT_FAIL:
+      // Serial.println("[Relay] INFO: Nothing to relay/receive!");
+      break;
+    default:
+      break;
+  }
+}
+
+uint32_t parse_dev_id(byte *payload, uint8_t len) {
+  if (payload == NULL)
+    return 0;
+  if (len < 11)
+    return 0;  // ToDo: Verify the minimum size of a LoRaWAN Packet
+
+  return ((payload[1]) | (payload[2] << 8) | (payload[3] << 16) | (payload[4] << 24));
+}
+
+void sleepRelay() {
+  digitalWrite(LS_GPS_ENABLE, LOW);
+  digitalWrite(LS_VERSION_ENABLE, LOW);
+  digitalWrite(LS_GPS_V_BCKP, LOW);
+  digitalWrite(SD_ON_OFF, LOW);
+
+  SPI.end();
+  delay(10);
+  STM32.stop();
+
+  SPI.begin();
+  Serial3.begin(115200);
+  //Serial.begin(115200);
+  Serial3.flush();
+}
